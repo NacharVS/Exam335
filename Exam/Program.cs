889023f62437ec5323af3967cd6a354a45d7d0f1
Khasanov335 <KhasanovFarit@mail.ru>
2020-12-26 09:22:21 +0300

    start

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -7,6 +7,8 @@ namespace Exam
         static void Main(string[] args)
         {
             Console.WriteLine("Leave hope behind all who enters here...");
+            Console.WriteLine( "Proverka");
+            Console.WriteLine("Proverka2");
         }
     }
 }
