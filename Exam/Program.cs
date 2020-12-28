8ec06d0f630db8b582760f8325063e1231f5eb8f
SadrievRinat <ris2002@mail.ru>
2020-12-28 08:43:15 +0300

    finish

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
 
 namespace Exam
 {
@@ -7,6 +9,48 @@ namespace Exam
         static void Main(string[] args)
         {
             Console.WriteLine("Leave hope behind all who enters here...");
+      
+
+
+        private string _name;
+        private string _surname;
+        private int _paymant;
+        private int _age;
+
+        
+        public string Regist(string newName, string newSurname, int year, int mouth, int day)
+        {
+            string akk = _surname + " " + _name + " " + _age + " ";
+            if (_age >= 18)
+            {
+                newName = newName.Trim();
+                var FL = newName[0];
+                var OL = newName.Remove(0, 1);
+                _name = FL.ToString().ToUpper() + OL.ToLower();
+                newSurname = newSurname.Trim();
+                var fl = newSurname[0];
+                var ol = newSurname.Remove(0, 1);
+                _surname = fl.ToString().ToUpper() + ol.ToLower();
+
+
+                int now = DateTime.Now.Year;
+                DateTime d = new DateTime(year, mouth, day);
+                _age = now - d.Year;
+             
+                return akk;
+            }
+            else { Console.WriteLine("Вам нет 18    "); }
+            return akk;
+
+
         }
+
+
+        
+        
+        
     }
+   
+
+ 
 }
