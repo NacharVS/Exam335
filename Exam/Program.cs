541b184e3606b0fc3a0b2901e85a6f357ae50bd1
Dinaremba <bot3.steam@mail.ru>
2020-12-28 08:32:03 +0300

    exam

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -6,7 +6,12 @@ namespace Exam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Leave hope behind all who enters here...");
+            BankAccount person1 = new BankAccount("Марат", 20000);
+            BankAccount person2 = new BankAccount("Денис", 30000);
+            person1.Transfer(person2);
+            Console.WriteLine(person2.money);
+            person2.Cut();
+            Console.WriteLine(person2.money);
         }
     }
 }
