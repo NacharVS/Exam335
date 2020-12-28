aa4e608158dd7007069adddab7b063953bf1ef5d
dimaagafetov <dimaagafetov2002@mail.ru>
2020-12-28 10:49:51 +0300

    exam

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
-
+using System.Threading;
 namespace Exam
 {
     class Program
     {
+        static void mythread1()
+        {
+            int[] arr = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = rnd.Next(0, 2);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void mythread2()
+        {
+            int[] arr = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = rnd.Next(-99, -9);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void mythread3()
+        {
+            int[] arr = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = rnd.Next(0, 10);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Leave hope behind all who enters here... ");
+            Thread thread1 = new Thread(mythread1);
+            Thread thread2 = new Thread(mythread2);
+            Thread thread3 = new Thread(mythread3);
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
         }
     }
 }
