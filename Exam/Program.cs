01e966b3bdb9bef79de430d96c51be0ce8d83a5b
batinov <batinov-nikita@mail.ru>
2020-12-28 08:45:01 +0300

    123

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -2,11 +2,43 @@
 
 namespace Exam
 {
-    class Program
+    class Exam
     {
-        static void Main(string[] args)
+        public string student;
+        public int rus;
+        public int matem;
+        public int inf;
+        private string v;
+
+        public Exam(string v)
         {
-            Console.WriteLine("Leave hope behind all who enters here...");
+            this.v = v;
+        }
+
+        public Exam(string student, int rus, int matem, int inf)
+        {
+            this.student = student;
+            this.rus = rus;
+            this.matem = matem;
+            this.inf = inf;
+        }
+        static void Main(string [] args)
+        {
+            Exam student1 = new Exam("Nikita,4,5,5");
+            Exam student2 = new Exam("Marat,5,5,5");
+            Exam student3 = new Exam("Ruslan,4,4,4");
+            Exam[] exams = { student1, student2, student3 };
+            Console.WriteLine("Введите число от 4-5");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n == 5)
+            {
+                Console.WriteLine("Поступил студент Марат");
+            }
+            else
+            {
+                Console.WriteLine("Поступили студенты Никита и Руслан ");
+            }
+
         }
     }
 }
